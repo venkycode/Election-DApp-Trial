61cabc9ea75830fcff0eb7dfb8ba0b926a6b7960
venkatesh <venkateshkotwade@gmail.com>
2019-07-06 23:51:59 +0530

    Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+/**
+ * The Election contract does this and that...
+ */
+contract Election {
+
+	string public candidate;
+
+	constructor() public{
+
+		candidate="Candidate1";
+
+		
+	}	
+}
